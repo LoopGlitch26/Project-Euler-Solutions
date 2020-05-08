277ee25a34611ffee1d94ba084a4c69a3df1369d
Bravish Ghosh <grandson.baba.2012@gmail.com>
2020-05-08 23:06:38 +0530

    Updated Solution w/o using loop

--- Java/P06.java
+++ Java/P06.java
@@ -13,6 +13,7 @@ public class P06
 {
 	public static void main(String[] args)
 	   {
+	       /* Previous Solution
 	       int s1=0,s2=0;
 	       for(int i = 1;i<=100;i++)
 	       {
@@ -20,6 +21,12 @@ public class P06
 	          s2+=(i*i);
 	        }
 	       int ans=(s1*s1)-s2;
-	       System.out.println(ans);
+	       System.out.println(ans); */
+		
+		//Updated Solution w/o for loop
+		int n=100, s1, s2;
+		s1=(n*(n+1)*(2*n+1))/6;
+		s2=(n*(n+1))/2;
+		System.out.println((s2*s2)-s1);
 	    }
 }
